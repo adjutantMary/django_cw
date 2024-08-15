be0ea1e7a0e6b4ca29fa17bc6296041c5201c5f0
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-08-16 00:52:43 +0300

    service models & start work with scheduler

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class MyAdminSite(admin.AdminSite):
+    site_header = "Информация о рассылках"
+
+mail_admin = MyAdminSite(name="mail-admin")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'body',]
+    
+@admin.register(MailingSettings)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ['start_time', 'end_time', 'period', 'status', 'message',]
+    date_hierarchy = 'start_time'
+    
+@admin.register(Attempt)
+class AttemtAdmin(admin.ModelAdmin):
+    list_display = ['mailing_settings', 'created_at', 
+                    'is_success', 'details',]
+    date_hierarchy = 'created_at'
+    
+mail_admin.register(Message, MessageAdmin)
+    
--- service/models.py
+++ service/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    '''Клиент сервиса'''
+    
+    email = models.EmailField(unique=True, verbose_name='почта')
+    name = models.CharField(max_length=128, blank=True, null=True)
+    comment = models.TextField(max_length=320, blank=True, null=True)
+    
+    
+class Message(models.Model):
+    '''Сообщение для рассылки'''
+    
+    subject = models.CharField(max_length=128, blank=True, null=True)
+    body = models.TextField(max_length=320, blank=True, null=True)
+    
+    
+class MailingSettings(models.Model):
+    '''Настройки рассылки'''
+    
+    class Period(models.TextChoices):
+        DAILY = 'D', 'Daily'
+        WEEKLY = 'W', 'Weekly'
+        MONTHLY = 'M', 'Monthly'
+    
+    class Status(models.TextChoices):
+        DONE = 'D', 'Done'
+        CREATED = 'C', 'Created'
+        LAUNCHED = 'L', 'Launched'
+        INACTIVE = 'I', 'Inactive'
+        
+    start_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    end_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    period = models.CharField(choices=Period.choices, default=Period.WEEKLY, max_length=2)
+    
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.INACTIVE, blank=True, null=True)
+    
+    clients = models.ManyToManyField(Client)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+ 
+    
+class Attempt(models.Model):
+    '''Попытка рассылки'''
+    
+    mailing_settings = models.ForeignKey(MailingSettings, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_success = models.BooleanField(default=False)
+    details = models.TextField(null=True, blank=True)
+    
