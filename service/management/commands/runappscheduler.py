ab865d07bc47c935a6f67ec8588468c6f46e53f1
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-08-16 21:04:55 +0300

    send mail command

--- service/management/commands/runappscheduler.py
+++ service/management/commands/runappscheduler.py
@@ -1,6 +1,10 @@
 import logging
+import smtplib
 
 from django.conf import settings
+from django.core.mail import send_mail
+from django.utils import timezone
+from smtplib import SMTP
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -8,14 +12,30 @@ from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
-from service.models import MailingSettings
+from service.models import MailingSettings, Attempt
 
 logger = logging.getLogger(__name__)
 
 
 def my_job():
-
-  pass
+  qs = MailingSettings.objects.filter(status='D')
+  for mailing in qs:
+    message = mailing.message
+    clients_mail = mailing.clients.values_list('mail', flat=True)
+    
+    try:
+      send_mail(
+        subject=message.subject,
+        message=message.body,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=clients_mail,
+        fail_silently=False
+      )
+      message.last_sent_time = timezone.now()
+    except smtplib.SMTPException as error:
+      Attempt.objects.create(mailing_settings=mailing, is_success=False, details=str(error))
+    else: 
+      Attempt.objects.create(mailing_settings=mailing, is_success=True)
 
 
 # The `close_old_connections` decorator ensures that database connections, that have become
