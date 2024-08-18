9e4e3969704e4261ff824f908d1d2f30919444f7
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-08-19 01:05:30 +0300

    client login views

--- service/views.py
+++ service/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
 
-def index(request):
-    return render(request, 'home.html')
+
+@login_required
+def client_list(request):
+    '''Отображение всех клиентов'''
+    clients = Client.objects.all()
+    return render(request, 'mailing_service/client_list.html', {'client': clients})
+
+@login_required
+def client_detail_list(request, pk):
+    '''Отображение детальной информации по клиенту'''
+    client = Client.objects.get(pk=pk)
+    return render(request, 'mailing_service/client_detail.html', {'client': client})
+
+@login_required
+def create_client(request):
+    '''Создание клиента'''
+    if request.method == "POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.owner = request.user
+            client.save()
+            return redirect('mailing:client_list')
+        
+    else:
+        form = ClientForm()
+    return render(request, 'mailing_service/client_form.html', {'form': form})
+
+@login_required
+def update_client(request, pk):
+    '''Обновление информации о клиенте'''
+    client = Client.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('mailing:client_list')
+        
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'mailing_service/client_form.html', {'form': form})
